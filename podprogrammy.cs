a3582aa2f9343a3053877fa2f1b9002dcefab6e8
Dinosau <43936528+Dinosau@users.noreply.github.com>
2019-09-06 00:44:55 +0300

    Update podprogrammy.cs

--- podprogrammy.cs
+++ podprogrammy.cs
@@ -1,4 +1,45 @@
-4.3
+
+
+4.1 Задача - нарисовать треугольник из звёздочек
+  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+  class Program
+  {
+      static void DrawPyramid(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                    Console.Write("*" + " ");
+                Console.WriteLine();
+            }
+        }
+      
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        DrawPyramid(a);
+    }
+  }
+}
+
+
+
+
+
+4.2
+  
+  
+  
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,3 +82,64 @@ namespace SoloLearn
     }
   }
 }
+
+
+
+
+4.3
+  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SoloLearn
+{
+  class Program
+  {
+      static void my_sqrt(out bool h,  out int sqrtN, int n)
+      {
+          int k = Convert.ToInt32(Math.Sqrt(n));
+          if(k * k == n)
+          {
+              h = true;
+              sqrtN = k;
+          }
+          else 
+          {
+              h = false;
+              sqrtN = 0;
+          }
+      }
+    static void Main(string[] args)
+    {
+        int n, a, b;
+        n = Convert.ToInt32(Console.ReadLine());
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        bool h;
+        int sqrtN;
+        if(n < 0)
+            n *= -1;
+        my_sqrt(out h, out sqrtN, n);
+        if(n == 0)
+            h = false;
+        if(h)
+            Console.WriteLine("true " + sqrtN);
+        else
+            Console.WriteLine("false");
+        while(a <= b)
+        {
+           // bool h1;
+            my_sqrt(out h, out sqrtN, a);
+            if(h)
+                Console.Write(a + " ");
+            ++a;
+        }
+    }
+  }
+}
+
+
